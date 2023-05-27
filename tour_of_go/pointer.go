95d8353864c9ba9451820f44724153b3253f2f6a
itachi-P <itachip38@gmail.com>
2023-05-27 23:56:56 +0900

    Modified pointer.go

--- tour_of_go/pointer.go
+++ tour_of_go/pointer.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
+//pointer:値のメモリアドレスを指す
+/*
+基本的にC言語と同じだが、ポインタ演算は無い
+
+ポインタの宣言(初期値はnilになる)
+var p *int
+&オペレータはそのオペランドへのポインタを引き出す
+i := 16
+p = &i
+fmt.Println(*p) //ポインタの指す実体値を読み出す
+*p = 256 		//ポインタを通してiへ値を代入する
+*/
+
 func main() {
+	var p *int //ポインタの宣言（初期化なし）
+	fmt.Println(p)
+
 	i, j := 42, 2701
 
-	p := &i         // point to i
+	p = &i          // point to i
 	fmt.Println(*p) // read i through the pointer
 	*p = 21         // set i through the pointer
 	fmt.Println(i)  // see the new value of i

799f2cc6a0d53d11e424b4e5b1b3a2dfa6ac982b
itachi-P <itachip38@gmail.com>
2023-05-29 23:18:40 +0900

    Exercise of Slices: Generate of bluescale images

---
+++ tour_of_go/exercise_slices.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+/*
+上記import文による外部モジュールの解決の為に、
+このファイル用のgo.modファイル生成、及び
+ワークスペースのルートにあるgo.workにモジュールを追加したが
+結局実行結果のIMAGE:の値はブラウザで表示しないと文字と記号の羅列
+大人しくA Tour of Goのサンドボックス上で実行した方がよさげ */
+
+func Pic(dx, dy int) [][]uint8 {
+
+	pic := make([][]uint8, dy)
+
+	for y := range pic {
+		pic[y] = make([]uint8, dx)
+		for x := range pic[y] {
+			pic[y][x] = uint8((x + y) / 2)
+		}
+	}
+
+	return pic
+}
+
+func main() {
+	pic.Show(Pic)
+}
---
+++ tour_of_go/go.mod
@@ -0,0 +1,5 @@
+module exercise_slices.go
+
+go 1.20
+
+require golang.org/x/tour v0.1.0

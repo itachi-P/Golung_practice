d32ea650bf3726411da28ce59703c8dcda588c9f
itachi-P <itachip38@gmail.com>
2023-06-03 22:51:07 +0900

    Added comments to Exercise 03 (Map).

--- tour_of_go/exercise02/exercise_slices.go
+++ tour_of_go/exercise02/exercise_slices.go
@@ -1,10 +1,12 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"golang.org/x/tour/pic"
+)
 
-/* 上記import文による外部モジュールの解決の為に、
+/* 上記import文による外部モジュールの依存問題解決の為に、
 このファイル用のgo.modファイル生成、及び
-ワークスペースのルートにあるgo.workにモジュールを追加したが
+ワークスペースのルートにある"go.work"にモジュールを追加したが
 結局実行結果のIMAGE:の値はブラウザで表示しないと文字と記号の羅列
 大人しくA Tour of Goのサンドボックス上で実行した方がよさげ */
 
@@ -15,7 +17,7 @@ func Pic(dx, dy int) [][]uint8 {
 	for y := range pic {
 		pic[y] = make([]uint8, dx)
 		for x := range pic[y] {
-			pic[y][x] = uint8((x * y) / 2)
+			pic[y][x] = uint8((x + y) / 2)
 		}
 	}
 
--- tour_of_go/exercise03/exercise_maps.go
+++ tour_of_go/exercise03/exercise_maps.go
@@ -7,23 +7,43 @@ import (
 	"golang.org/x/tour/wc"
 )
 
+/* A Tour of Go : Exercise 03(Maps)
+https://go-tour-jp.appspot.com/moretypes/23
+WordCount関数の実装
+string s で渡される文章の、各単語の出現回数のmapを返す
+（スペース区切りの英文であることが前提）
+用意されているInput（一例）:
+"I ate a donut. Then I ate another donut."
+上記のInputに対し、戻り値を下記のようなMapに加工する:
+map[string]int{"I": 2, "ate": 2, "a": 1, "donut.": 2, "Then": 1, "another": 1,}
+*/
+
 func WordCount(s string) map[string]int {
+	//公式が用意してある英文をまずはスペースで区切りスライスに分解
 	words := strings.Fields(s)
 	fmt.Printf("%q\n", words)
+	//戻り値として返すMapを準備
 	m := make(map[string]int)
 
 	for _, v := range words {
+		//同じ単語の出現回数（基本は1）
 		count := 0
-		if v, ok := m[v]; ok {
+		//もし既にKeyが存在すれば2以上にカウントアップする
+		if v, ok := m[v]; ok { //2つ目は変数ok == trueの省略記法
 			count = v
 		}
+		//MapにKey（単語）とValue（出現回数）を追加または上書きする
 		m[v] = count + 1
-
-		fmt.Println(v)
+		//fmt.Println(v)
 	}
 	return m
 }
 
 func main() {
+	/*問題を解く上で、この外部パッケージ関数の中身の理解が必要
+	https://github.com/golang/tour/blob/master/wc/wc.go
+	Test関数によりWordCount関数が期待される挙動と一致するか判定される
+	引数(s string)となる英文は自動的に与えられる
+	*/
 	wc.Test(WordCount)
 }
